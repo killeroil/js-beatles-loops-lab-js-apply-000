dc6548007de3814cb23dcff58674114f9dc777a0
ZHENGJIANLIU <782059891@qq.com>
2020-07-19 19:38:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function iLoveTheBeatles(number){
   do{
     array.push('I love the Beatles!')
     i++
-  }while(number < 15 && i<= 15 - number)
+  }while(number < 15 && i <= number)
   return array
 }

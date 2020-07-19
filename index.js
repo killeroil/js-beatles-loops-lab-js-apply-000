fbc3f312c8b1e3885ca38bc0a26760eaad1946a3
ZHENGJIANLIU <782059891@qq.com>
2020-07-19 19:28:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var p = []
+  for(var i = 0; i <musicians.length; i++){
+    p.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return p
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  while(i<facts.length){
+    facts[i] += '!!!'
+    i++
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  var array = []
+  do{
+    array.push('')
+  }
+  
+}
